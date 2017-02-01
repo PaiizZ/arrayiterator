d1da210568ec3ffff841f3b1719f795bc552f05d
paiizz <wanchanapon.t@ku.th>
2017-02-01 14:43:19 +0700

    create Main for test interator

---
+++ src/ku/util/Main.java
@@ -0,0 +1,25 @@
+package ku.util;
+
+/**
+ * This main class is for checking that ArrayIterator class is working correctly.
+ *
+ * @author Wanchananpon Thanwaranurak
+ * @version 2/1/2017 AD.
+ */
+
+public class Main {
+    public static void main(String[] arg) {
+        String[] array = {"apple", "banana", null, "carrot"};
+
+        ArrayIterator<String> iter = new ArrayIterator(array);
+
+        System.out.println(iter.next());
+        System.out.println(iter.hasNext());
+        System.out.println(iter.hasNext());
+        System.out.println(iter.hasNext());
+        System.out.println(iter.next());
+        System.out.println(iter.next());
+        System.out.println(iter.hasNext());
+        System.out.println(iter.next());
+    }
+}
